66732ff10550d2dca33509e0e485a07d0f5bc9c6
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2014-01-23 11:20:39 +0100

    Attempt at faster implementation, not correct yet

--- src/hclust.jl
+++ src/hclust.jl
@@ -42,6 +42,60 @@ function hclust{T}(d::Matrix{T}, method::Function)
     hcat(mr, mc), h
 end
 
+## Nearest Neighbor Chain algorithm, see Wikipedia
+## It doesn't give me the same results, there must be something I miss. 
+function hclust1{T}(d::Matrix{T}, method::Function)
+    @assert size(d,1) == size(d,2)
+    if !issym(d)
+        d += d'                 # replaces d, which must be symmetric
+    end
+    mr = Int[]                  # min row
+    mc = Int[]                  # min col
+    h = T[]                     # height
+    nc = size(d,1)
+    cl = [1:nc]
+    S = (Int,Vector{Int})[]     # stack of ID and cluster
+    next = 1
+    while next < nc
+        if length(S) == 0
+            id = shift!(cl)
+            push!(S, (-id,[id]))
+        end
+        id, C = last(S)
+        ## compute distance to single clusters
+        min = Inf
+        mi = 0
+        for i in 1:length(cl)
+            distance = method(d[cl[i],C])
+            if distance < min
+                mi = i
+                min = distance
+            end
+        end
+        ## and compare that to previous cluster on stack
+        if mi>0
+            print("Minimum element ", cl[mi], " at ", min, " with C ", C')
+        end
+        if length(S) > 1 && (distance = method(d[S[end-1][2], C])) < min
+            ## merge top two on stack
+            id1, c1 = pop!(S)
+            id2, c2 = pop!(S)
+            println("Merging ", id1, " and ", id2, " at ", distance)
+            new = sort(vcat(c1, c2))
+            push!(S, (next,new))
+            push!(mr, id1)
+            push!(mc, id2)
+            push!(h, distance)
+            next += 1
+        else
+            id = splice!(cl, mi)
+            push!(S, (-id,[id]))
+        end
+    end
+    o = sortperm(h)
+    (hcat(mr, mc), h)
+end
+
 function hclust{T}(d::Matrix{T}, method::Symbol)
     if method == :complete
         h = hclust(d, maximum)
