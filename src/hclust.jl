99bfa9e8fa0bc79c0b24ae9c4c2c0902c9397377
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2014-01-27 20:17:17 +0100

    hclust_minimum now debugged, and faster than R's!

--- src/hclust.jl
+++ src/hclust.jl
@@ -1,11 +1,13 @@
 ## hclust.jl (c) 2014 David A. van Leeuwen 
 ## Hierarchical clustering, similar to R's hclust()
 
+## Algorithms are based upon C. F. Olson, Parallel Computing 21 (1995) 1313--1325. 
+
 require("types.jl")
 
 ## This seems to work like R's implementation, but it is extremely inefficient
 ## This probably scales O(n^3) or worse. We can use it to check correctness
-function hclust{T}(d::Matrix{T}, method::Function)
+function hclust_n3{T}(d::Matrix{T}, method::Function)
     @assert size(d,1) == size(d,2)
     if !issym(d)
         d += d'                 # replaces d, which must be symmetric
@@ -43,6 +45,7 @@ function hclust{T}(d::Matrix{T}, method::Function)
     hcat(mr, mc), h
 end
 
+## functions to comput maximum, minimum, mean for just a slice of an array
 function Base.maximum{T}(d::Matrix{T}, cl1::Vector{Int}, cl2::Vector{Int})
     max = -Inf
     mi = mj = 0
@@ -218,15 +221,16 @@ function hclust2{T}(d::Matrix{T}, method::Function)
     hcat(mr[o], mc[o]), h[o]
 end
 
-## Efficient single link algorithm, according to Olson, O(n^2)
+## Efficient single link algorithm, according to Olson, O(n^2), fig 2. 
 ## For each i < j compute D(i,j) (this is already given)
 ## For each 0 < i <= n compute Nearest Neighbor N(i)
 ## Repeat n-1 times
-##  find i,j that minimize D(i,j)
-##  merge clusters i and j
-##  update D(i,j) and N(i) accordingly
+##   find i,j that minimize D(i,j)
+##   merge clusters i and j
+##   update D(i,j) and N(i) accordingly
 function hclust_minimum{T}(d::Matrix{T})
     @assert size(d,1) == size(d,2)
+    ## For each i < j compute d[i,j] (this is already given) --- we need a local copy
     if !issym(d)
         d += d'                
     else
@@ -236,77 +240,134 @@ function hclust_minimum{T}(d::Matrix{T})
     mc = Int[]                  # min col
     h = T[]                     # height
     nc = size(d,1)
-    cl = map(x->[x], 1:nc)
     merges = -[1:nc]
     next = 1
-    ## I must leave d(i,i) out!
-    d += diagm(Inf*ones(nc))
-    N = map(i->indmin(d[i,:]), 1:nc) # nearest neighbors, N[i] is nearest neighbor of cluster i
-    while nc > 1                     # O(n)
+    ## For each 0 < i <= n compute Nearest Neighbor N[i]
+    N = zeros(Int, nc)
+    for k = 1:nc
+        min = Inf
+        mk = 0
+        for i = 1:(k-1)
+            if d[i,k] < min
+                min = d[i,k]
+                mk = i
+            end
+        end
+        for j = (k+1):nc
+            if d[k,j] < min
+                min = d[k,j]
+                mk = j
+            end
+        end
+        N[k] = mk
+    end
+#    printupper(d)
+#    for n in N[1:nc] print(n, " ") end; println()
+    lastmin = -Inf
+    ## the main loop
+    while nc > 1                # O(n)
         min = d[1,N[1]]
         i = 1
         for k in 2:nc           # O(n)
-            distance = d[k,N[k]]            
+            if k < N[k]
+                distance = d[k,N[k]]            
+            else
+                distance = d[N[k],k]
+            end
             if distance < min
                 min = distance 
                 i = k
             end
         end
+        if (min < lastmin)
+            error("Not finding consectutive minima ", min, " ", lastmin, " ", next)
+        end
+        lastmin = min
         j = N[i]
         if i > j
             i, j = j, i     # make sure i < j
         end
-        ## merge clusters i and j
-        cl[i] = vcat(cl[i], cl[j])
-        cl[j] = cl[nc]
-        ## update result
-        push!(mr, merges[i])
-        push!(mc, merges[j])
+#        println("Merging ", i, " and ", j)
+        ## update result, compatible to R's order.  It must be possible to do this simpler than this...
+        if merges[i] < 0 && merges[j] < 0 && merges[i] > merges[j] ||
+            merges[i] > 0 && merges[j] > 0 && merges[i] < merges[j] ||
+            merges[i] < 0 && merges[j] > 0
+            push!(mr, merges[i])
+            push!(mc, merges[j])
+        else
+            push!(mr, merges[j])
+            push!(mc, merges[i])
+        end
         push!(h, min)
         merges[i] = next
         merges[j] = merges[nc]
-        ## update d[m,k] for k != i or j, m = i or j
-        for k = 1:nc if k != i && k != j # O(n)
-            if d[i,k] < d[j,k]
-                d[j,k] = d[i,k]
-            else
+        ## update d, split in ranges k<i, i<k<j, j<k<=nc
+        for k = 1:(i-1)         # k < i
+            if d[k,i] > d[k,j]
+                d[k,i] = d[k,j]
+            end
+        end
+        for k = (i+1):(j-1)     # i < k < j
+            if d[i,k] > d[k,j]
+                d[i,k] = d[k,j]
+            end
+        end
+        for k = (j+1):nc        # j < k <= nc
+            if d[i,k] > d[j,k]
                 d[i,k] = d[j,k]
             end
-        end end
-        for k = 1:nc
-            d[k,i] = d[i,k]   # keep d symmetric
         end
-        ## update N[k] for k != i
+        ## move the last row/col into j
+        for k=1:(j-1)           # k==nc, 
+            d[k,j] = d[k,nc]
+        end
+        for k=(j+1):(nc-1)
+            d[j,k] = d[k,nc]
+        end
+ #       printupper(d[1:(nc-1),1:(nc-1)])
+        ## update N[k], k !in (i,j)
         for k=1:nc
             if N[k] == j       # update nearest neigbors != i
                 N[k] = i
             elseif N[k] == nc
                 N[k] = j
             end
-            d[j,k] = d[k,j] = d[nc,k] # update row/column j
         end
+        N[j] = N[nc]            # update N[j]
         ## update nc, next
         nc -= 1
         next += 1
         ## finally we need to update N[i], because it was nearest to j
-        N[i] = indmin(d[i,1:nc])
-    end
-    o = sortperm(h)
-    io = invperm(o)
-    for i in 1:length(mr)
-        if mr[i] > 0 
-            mr[i] = io[mr[i]]
+        min = Inf
+        mk = 0
+        for k=1:(i-1)
+            if d[k,i] < min
+                min = d[k,i]
+                mk = k
+            end
         end
-        if mc[i] > 0
-            mc[i] = io[mc[i]]
+        for k = (i+1):nc
+            if d[i,k] < min
+                min = d[i,k]
+                mk = k
+            end
         end
+        N[i] = mk
+#        for n in N[1:nc] print(n, " ") end; println()
     end
-    hcat(mr[o], mc[o]), h[o]
-##    hcat(mr, mc), h
+    return hcat(mr, mc), h
 end
         
-        
-                
+function printupper(d::Matrix)
+    n = size(d,1)
+    for i = 1:(n-1)
+        print(" " ^ ((i-1) * 6))
+        for j = (i+1):n
+            print(@sprintf("%5.2f ", d[i,j]))
+        end
+        println()
+    end
+end                
     
 
 function hclust{T}(d::Matrix{T}, method::Symbol)
@@ -326,7 +387,10 @@ end
 function test_hclust(N::Int)
     d = gendist(N)
     writedlm("hclust.txt", d)
-    hclust(d, :minimum)
+    h = hclust_minimum(d)
+    writedlm("merge.txt", h[1])
+    writedlm("height.txt", h[2])
+    d
 end
 
 function gendist(N::Int)
