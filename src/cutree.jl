5db94811b69f6ad430d95f5dbaa697706f8c24c6
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2014-03-17 13:33:53 +0100

    Well, this adds cutree.jl

---
+++ src/cutree.jl
@@ -0,0 +1,33 @@
+## cut a tree at height `h' or to `k' clusters
+function cutree(hclust::Hclust; k::Int=1, 
+                h::Real=maximum(hclust.height))
+    clusters = Vector{Int}[]
+    nnodes = length(hclust.labels)
+    nodes = [[i::Int] for i=1:nnodes]
+    N = nnodes - k
+    i = 1
+    while i<=N && hclust.height[i] <= h
+        both = vec(hclust.merge[i,:])
+        new = Int[]
+            for x in both
+                if x<0
+                    push!(new, -x)
+                    nodes[-x] = []
+                else
+                    append!(new, clusters[x])
+                    clusters[x] = []
+                end
+            end
+        push!(clusters, new)
+        i += 1
+    end
+    all = vcat(clusters, nodes)
+    all = all[map(length, all) .> 0]
+    ## convert to a single array of cluster indices
+    res = Array(Int, nnodes)
+    for (i,cl) in enumerate(all)
+        res[cl] = i
+    end
+    res
+end
+
