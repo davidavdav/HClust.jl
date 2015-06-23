22d49f2cc5dadc44f633d462e98c420a9d92ce63
Scott Lundberg <scottmlundberg@gmail.com>
2015-06-23 13:00:34 -0700

    Added unit tests in Julia for all clustering types.

---
+++ test/runtests.jl
@@ -0,0 +1,18 @@
+using HClust
+using Base.Test
+
+# load the examples array
+include("generated_examples.jl")
+
+# test to make sure many random examples match R's implementation
+for example in examples
+	h = hclust(example["D"], example["method"])
+	for i in 1:size(h.merge)[2]
+		for j in 1:size(h.merge)[1]
+			@test h.merge[j,i] == example["merge"][j,i]
+		end
+	end
+	for i in 1:length(h.height)
+		@test_approx_eq_eps h.height[i] example["height"][i] 1e-5
+	end
+end
