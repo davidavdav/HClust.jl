a5e52a5c478ec921176ffc0311aaebee62791782
David A. van Leeuwen <david.vanleeuwen@gmail.com>
2015-07-02 07:55:20 +0200

    Fix bug found by @slundberg in PR #2 occurring at small cluster sizes

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,13 +6,14 @@ include("generated_examples.jl")
 
 # test to make sure many random examples match R's implementation
 for example in examples
-	h = hclust(example["D"], example["method"])
-	for i in 1:size(h.merge)[2]
-		for j in 1:size(h.merge)[1]
-			@test h.merge[j,i] == example["merge"][j,i]
-		end
+#    println("Example: ", example)
+    h = hclust(example["D"], example["method"])
+    for i in 1:size(h.merge)[2]
+	for j in 1:size(h.merge)[1]
+	    @test h.merge[j,i] == example["merge"][j,i]
 	end
-	for i in 1:length(h.height)
-		@test_approx_eq_eps h.height[i] example["height"][i] 1e-5
-	end
+    end
+    for i in 1:length(h.height)
+	@test_approx_eq_eps h.height[i] example["height"][i] 1e-5
+    end
 end
